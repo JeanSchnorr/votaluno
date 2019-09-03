63718bb5df7e99c0db4b13b4199223b8693d0fff
JeanSchnorr <jeanschnorr2000@gmail.com>
2019-09-03 19:25:24 -0400

    Integrando Login

---
+++ votaluno/usuarios/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.contrib.auth.base_user import BaseUserManager
+
---
+++ votaluno/usuarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path ,include
+from django.contrib.auth import views as auth_views
+
+app_name = 'usuarios'
+
+urlpatterns = [
+    path('sair/', auth_views.LogoutView.as_view(template_name='teste.html'), name='sair'),
+    path('entrar/', auth_views.LoginView.as_view(template_name='login.html'), name='entrar'),
+] 
