ea0b89d9d42c9cdcc55b9336ff7d989da42e576a
JeanSchnorr <jeanschnorr2000@gmail.com>
2019-09-12 08:32:09 -0400

    Adicionando models

--- votaluno/votacoes/admin.py
+++ votaluno/votacoes/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Turma
+from .models import Turma, Aluno, Votacao,Voto,Avaliacao,Conselho
 
 admin.site.register(Turma);
+admin.site.register(Aluno);
+admin.site.register(Conselho);
+admin.site.register(Avaliacao);
+admin.site.register(Votacao);
+admin.site.register(Voto);
--- votaluno/votacoes/models.py
+++ votaluno/votacoes/models.py
@@ -1,9 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Turma(models.Model):
-    Curso = models.TextField(max_length=30)
-    Ano = models.IntegerField()
-    Sala = models.TextField(max_length=1)
+    curso = models.TextField(max_length=30)
+    ano = models.IntegerField()
+    sala = models.TextField(max_length=1)
 
     def __str__(self):
-        return f'{self.Ano}º "{self.Sala}" {self.Curso}'
+        return f'{self.ano}º "{self.sala}" {self.curso}'
+
+class Aluno(models.Model):
+    nome = models.TextField(max_length=50)
+    cpf = models.TextField(max_length=14)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
+
+class Avaliacao(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    aluno = models.ForeignKey("Aluno", on_delete=models.CASCADE)
+    bimestre = models.IntegerField()
+    #ano
+    
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+
+    def __str__(self):
+        return f'{self.aluno} - {self.usuario}'
+    
+class Conselho(models.Model):
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+    data = models.DateField(auto_now=False)
+    situacao = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.turma
+
+class Votacao(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    situacao = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Votação'
+        verbose_name_plural = 'Votações'
+
+class Voto(models.Model):
+    SITUACAO_CHOICES={
+        ('Aprovar', 'Aprovar'),   
+        ('Reprovar', 'Reprovar'),   
+        ('Abster', 'Abster')
+    }
+    votacao = models.ForeignKey(Votacao, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    situacao = models.CharField(max_length=8, choices=SITUACAO_CHOICES, default='Abster')
