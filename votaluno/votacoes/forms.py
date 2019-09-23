a50a4d3f8d02d8b8002de1348ae791669635e797
JeanSchnorr <jeanschnorr2000@gmail.com>
2019-09-23 19:46:29 -0400

    Adicionando licença MIT

--- votaluno/votacoes/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import AvaliacaoTurma
-
-class AvaliacaoTurmaForm(forms.Form):
-    avaliacoes = AvaliacaoTurma.objects.filter(usuario=request.user)[:10]
-
--- votaluno/votacoes/views.py
+++ votaluno/votacoes/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from .models import Aluno, AvaliacaoTurma
+from .models import Aluno, AvaliacaoTurma, Turma
 from django.shortcuts import render
-from .forms import avalia
 
 @login_required
 def home(request):
@@ -11,18 +10,13 @@ def home(request):
 
 def avaliacoesTurmas(request):
   context = {}
+  turmasSemAvaliacoes = Turma.objects.filter()
   avaliacoes = AvaliacaoTurma.objects.filter(usuario=request.user)[:10]
   context['avaliacoes'] = avaliacoes
   return render(request,'avaliacoes/avaliacoesTurmas.html',context)
 
-@require_POST
 def criarAvaliacaoTurma(request):
-  form = AvaliacaoTurmaForm(request.POST)
-  if form.is_valid():
-    novaAvaliacao = AvaliacaoTurma(usuario=request.user, turma=request.POST['Turma'],bimestre=request.POST['Bimestre'],ano=request.POST['ano'])
-
-    novaAvaliacao.save()
-  return avaliacoesTurmas
+  return redirect('avaliacoes/avaliacoesTurmas.html')
 
 def admin(request):
   return HttpResponseRedirect('/admin')
