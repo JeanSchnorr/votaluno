c7c4d98d08affba296c6e41f30724304946eaee0
Jean Schnorr <jeanschnorr2000@gmail.com>
2019-12-04 00:37:56 -0400

    Debug = false

--- votaluno/settings.py
+++ votaluno/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1@=512%wk^1^*3t1x&8)z(51ob((ihikmwnzak@e7@ab3+bh1j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
