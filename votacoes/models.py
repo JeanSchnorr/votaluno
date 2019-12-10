05f215a9ad466f1f8a463f90cd61cef63d5a244a
Jean Schnorr <jeanschnorr2000@gmail.com>
2019-12-09 20:38:39 -0400

    criando página faq

--- votacoes/models.py
+++ votacoes/models.py
@@ -114,4 +114,4 @@ class Voto(models.Model):
     situacao = models.CharField(max_length=8, choices=SITUACAO_CHOICES, default='Abster')
     votado = models.BooleanField(default=False)
     def __str__(self):
-        return f'{self.usuario} para {self.votacao}'
+        return f'{self.usuario} para {self.votacao}' 
